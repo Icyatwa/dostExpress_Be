627dfc91fb9027a2ad999b47697017b9853b0cce
Icyatwa <icyatwandoba@gmail.com>
2024-05-16 11:24:31 -1100

    cyusa

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,3 @@
-// const User = require('../models/userModel');
-// const jwt = require('jsonwebtoken');
-
-// const createToken = (_id) => {
-//   return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' });
-// };
-
-// const loginUser = async (req, res) => {
-//   const { email, password } = req.body;
-//   try {
-//     const user = await User.login(email, password);
-//     const token = createToken(user._id);
-//     res.status(200).json({ email, token });
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// };
-
-// const signupUser = async (req, res) => {
-//   const { email, password, companyName, accessCode } = req.body;
-//   try {
-//     const user = await User.signup(email, password, companyName, accessCode);
-//     const token = createToken(user._id);
-//     res.status(200).json({ email, token });
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// };
-
-// module.exports = { signupUser, loginUser };
-
 
 // userController.js
 const User = require('../models/userModel');
@@ -54,7 +23,7 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password, companyName, accessCode);
     const token = createToken(user._id);
-    res.status(200).json({ email, token });
+    res.status(200).json({ email, token }); 
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
