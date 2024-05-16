1f980342440d16008a42cbfe8a48a96db161cc94
Icyatwa <icyatwandoba@gmail.com>
2024-05-16 11:20:10 -1100

    user model

--- models/userModel.js
+++ models/userModel.js
@@ -238,7 +238,7 @@ userSchema.statics.signup = async function(email, password, companyName, accessC
     'Fidelity Express': 'FDTY2024',
     'Yahoo Car Express': 'YCEX2024',
     'Alpha Express': 'ALPX2024',
-    'Muhamad': 'MUHAM2024',
+    'Rudah': 'RUDAH2024',
   };
 
   if (companyAccessCodes[companyName] !== accessCode) {
